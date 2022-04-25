8de16bc396af47357f283278c3acc5b8b01d0320
rasset69@gmail.com <rasset69@gmail.com>
2022-04-25 10:17:44 -0600

    Add project files.

---
+++ SethRasm_Etch_o_Sketch/SethRas_Etcho_A_Sketch.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SethRas_Etcho_A_Sketch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DrawPictureBox = New System.Windows.Forms.PictureBox()
+        Me.DrawTANButton = New System.Windows.Forms.Button()
+        Me.DrawCosButton = New System.Windows.Forms.Button()
+        Me.DrawSINButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.ToolStripMenu = New System.Windows.Forms.ToolStrip()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DrawPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DrawPictureBox
+        '
+        Me.DrawPictureBox.Location = New System.Drawing.Point(0, 0)
+        Me.DrawPictureBox.Name = "DrawPictureBox"
+        Me.DrawPictureBox.Size = New System.Drawing.Size(542, 310)
+        Me.DrawPictureBox.TabIndex = 0
+        Me.DrawPictureBox.TabStop = False
+        '
+        'DrawTANButton
+        '
+        Me.DrawTANButton.Location = New System.Drawing.Point(0, 316)
+        Me.DrawTANButton.Name = "DrawTANButton"
+        Me.DrawTANButton.Size = New System.Drawing.Size(92, 23)
+        Me.DrawTANButton.TabIndex = 1
+        Me.DrawTANButton.Text = "Draw TAN"
+        Me.DrawTANButton.UseVisualStyleBackColor = True
+        '
+        'DrawCosButton
+        '
+        Me.DrawCosButton.Location = New System.Drawing.Point(141, 316)
+        Me.DrawCosButton.Name = "DrawCosButton"
+        Me.DrawCosButton.Size = New System.Drawing.Size(96, 23)
+        Me.DrawCosButton.TabIndex = 2
+        Me.DrawCosButton.Text = "Draw Cosine"
+        Me.DrawCosButton.UseVisualStyleBackColor = True
+        '
+        'DrawSINButton
+        '
+        Me.DrawSINButton.Location = New System.Drawing.Point(280, 316)
+        Me.DrawSINButton.Name = "DrawSINButton"
+        Me.DrawSINButton.Size = New System.Drawing.Size(88, 23)
+        Me.DrawSINButton.TabIndex = 3
+        Me.DrawSINButton.Text = "Draw Sin"
+        Me.DrawSINButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(0, 345)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(368, 76)
+        Me.ClearButton.TabIndex = 4
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ToolStripMenu
+        '
+        Me.ToolStripMenu.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ToolStripMenu.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStripMenu.Name = "ToolStripMenu"
+        Me.ToolStripMenu.Size = New System.Drawing.Size(1269, 25)
+        Me.ToolStripMenu.TabIndex = 5
+        Me.ToolStripMenu.Text = "ToolStrip1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(137, 427)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 6
+        '
+        'SethRas_Etcho_A_Sketch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1269, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ToolStripMenu)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.DrawSINButton)
+        Me.Controls.Add(Me.DrawCosButton)
+        Me.Controls.Add(Me.DrawTANButton)
+        Me.Controls.Add(Me.DrawPictureBox)
+        Me.Name = "SethRas_Etcho_A_Sketch"
+        Me.Text = "Form1"
+        CType(Me.DrawPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DrawPictureBox As PictureBox
+    Friend WithEvents DrawTANButton As Button
+    Friend WithEvents DrawCosButton As Button
+    Friend WithEvents DrawSINButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents ToolStripMenu As ToolStrip
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ SethRasm_Etch_o_Sketch/SethRas_Etcho_A_Sketch.vb
@@ -0,0 +1,186 @@
+﻿Option Strict On
+Option Explicit On
+Imports System.Math
+
+Public Class SethRas_Etcho_A_Sketch
+    Dim currentColor As Color
+    Dim currentPenSize As Integer
+    'Upon Startup make the pen black and a size of 2
+    Private Sub SethRas_Etcho_A_Sketch_Load(sender As Object, e As EventArgs) Handles Me.Load
+        currentColor = Color.Black
+        currentPenSize = 1
+    End Sub
+    'Start the program and tell it you want to draw something with the Graphics function
+    'Draw on the picturebox and not the rest of the form
+    'recall pensize
+    'When drawing there needs to be a starting point and an endpoint define them
+    'When done get rid of the pen and stop the graphics
+    Sub Sketch(startX As Integer, startY As Integer, endX As Integer, endY As Integer)
+        Dim g As Graphics = DrawPictureBox.CreateGraphics
+        Dim pen As New Pen(Me.currentColor)
+        pen.Width = currentPenSize
+        g.DrawLine(pen, startX, startY, endX, endY)
+        g.Dispose()
+        pen.Dispose()
+    End Sub
+
+    'Place a colorDialog on the Form 
+    'Use it to pick the pen color, we will call this with another button under mouse functions
+    Sub PickPenColor()
+        ColorDialog1.ShowDialog()
+        Me.currentColor = ColorDialog1.Color
+    End Sub
+
+    'Here we select what button draws so the form doesnt draw with the mouse move but with the left button
+    'Call the middle mouse button as the color pallet selector
+    'have the draw function draw from old to new coordinates
+    Private Sub GraphicsForm_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawPictureBox.MouseMove, DrawPictureBox.MouseDown
+        Static oldX, oldY As Integer
+        Me.Text = $"({e.X},{e.Y}) Button:{e.Button.ToString()}"
+        Select Case e.Button.ToString
+            Case "Left"
+                Sketch(oldX, oldY, e.X, e.Y)
+            Case "Middle"
+                PickPenColor()
+        End Select
+        oldX = e.X
+        oldY = e.Y
+    End Sub
+
+    'Refresh the picture box on clear
+    Sub Clear()
+        DrawPictureBox.Refresh()
+    End Sub
+    'move the form slightly to add an 'animation' of shaking
+    Sub shake()
+        Dim offset As Integer = 30
+        For i = 0 To 10
+            Me.Left += offset
+            Me.Top += offset
+            offset *= -1 'invert offset
+            System.Threading.Thread.Sleep(200) ' wait 200ms
+        Next
+    End Sub
+    'Assign clear subs to the clear button
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        shake()
+        Clear()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox1.Text = CStr(currentPenSize)
+        Try
+            currentPenSize = CInt(TextBox1.Text)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+
+
+    Sub DrawSinWave()
+        Dim x, y, ymax, oldY, oldX As Integer
+        Dim oldColor As Color = Me.currentColor
+        Me.currentColor = Color.LightGreen
+        ymax = 100
+        x = 45
+        oldY = ymax
+        For x = 0 To 360 Step 1
+
+            'use system.math
+            'degrees must be converted to radians deg * (PI/180)
+            'Draw sin
+            y = CInt(ymax * Sin(x * (PI / 180)) * -1) + ymax + 25
+            'y = CInt(Round(ymax * Sin(x * (PI / 180))) * -1) + ymax
+            'y = CInt(Ceiling(ymax * Sin(x * (PI / 180))) * -1) + ymax
+            'y = CInt(Floor(ymax * Sin(x * (PI / 180))) * -1) + ymax
+
+            Sketch(oldX, oldY, x, y)
+            'Console.WriteLine($"({x},{y})")
+            oldX = x
+            oldY = y
+
+        Next
+        Me.currentColor = oldColor
+
+    End Sub
+
+    Private Sub DrawSINButton_Click(sender As Object, e As EventArgs) Handles DrawSINButton.Click
+        Dim g As Graphics = DrawPictureBox.CreateGraphics
+        Dim pen As New Pen(Me.currentColor)
+        pen.Width = currentPenSize
+        g.Dispose()
+        pen.Dispose()
+        DrawSinWave()
+    End Sub
+
+    Sub DrawCosWave()
+        Dim x, y, ymax, oldY, oldX As Integer
+        Dim oldColor As Color = Me.currentColor
+        Me.currentColor = Color.LightBlue
+        ymax = 100
+        x = 45
+        oldY = ymax
+        For x = 0 To 360 Step 1
+
+            'use system.math
+            'degrees must be converted to radians deg * (PI/180)
+
+            'Draw cos
+            y = CInt(ymax * Cos(x * (PI / 180)) * -1) + ymax + 25
+
+            Sketch(oldX, oldY, x, y)
+            'Console.WriteLine($"({x},{y})")
+            oldX = x
+            oldY = y
+
+        Next
+        Me.currentColor = oldColor
+
+    End Sub
+
+    Private Sub DRAWCOSBUTTON_CLICK(sender As Object, e As EventArgs) Handles DrawCOSButton.Click
+        Dim g As Graphics = DrawPictureBox.CreateGraphics
+        Dim pen As New Pen(Me.currentColor)
+        pen.Width = currentPenSize
+        g.Dispose()
+        pen.Dispose()
+        DrawCosWave()
+    End Sub
+
+    Sub DrawTanWave()
+        Dim x, y, ymax, oldY, oldX As Integer
+        Dim oldColor As Color = Me.currentColor
+        Me.currentColor = Color.Red
+        ymax = 100
+        x = 45
+        oldY = ymax
+
+        For x = 0 To 360 Step 1
+            'use system.math
+            'degrees must be converted to radians deg * (PI/180)
+
+            'Draw tan
+            Try
+                'don't crash on +- infinite
+                y = CInt(ymax * Tan(x * (PI / 180)) * -1) + ymax + 25
+                Sketch(oldX, oldY, x, y)
+                oldY = y
+                oldX = x
+            Catch ex As Exception
+                'Console.WriteLine($"oops infinite is too hard to handle!")
+                'Console.WriteLine(ex.Message)
+            End Try
+
+        Next
+        Me.currentColor = oldColor
+
+    End Sub
+    Private Sub DRAWTANBUTTON_CLICK(sender As Object, e As EventArgs) Handles DrawTANButton.Click
+        Dim g As Graphics = DrawPictureBox.CreateGraphics
+        Dim pen As New Pen(Me.currentColor)
+        pen.Width = currentPenSize
+        g.Dispose()
+        pen.Dispose()
+        DrawTanWave()
+    End Sub
+End Class
